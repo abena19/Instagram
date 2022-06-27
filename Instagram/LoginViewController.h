e69ef669dd01fa67cc2f89f31bd66edde7bc42e9
abena19 <ofosuabena19@gmail.com>
2022-06-27 16:39:59 -0700

    Implement user signup and login features using Parse authentication

---
+++ Instagram/LoginViewController.h
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.h
+//  Instagram
+//
+//  Created by Abena Ofosu on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+- (IBAction)registerUser:(id)sender;
+- (IBAction)loginUser:(id)sender;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
